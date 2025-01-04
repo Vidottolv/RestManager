dd45297c9547cb11eeb2aa0a61ff68258b99032f
Lucas Vidotto <lucasvidotto12@gmail.com>
2025-01-04 11:42:47 -0300

    update UserController

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestManager.Models;
 using RestManager.Services;
 
 namespace RestManager.Controller
@@ -15,6 +17,119 @@ namespace RestManager.Controller
             _userService = userService;
         }
 
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IAsyncEnumerable<User>>> GetUsers()
+        {
+            try
+            {
+                var users = await _userService.GetUsers();
+                return Ok(users);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro na hora de consultar os usuarios");
+            }
+        }
 
+        [HttpGet("UserName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IAsyncEnumerable<User>>> GetUserByName([FromQuery] string name)
+        {
+
+            try
+            {
+                var users = await _userService.GetUserByName(name);
+                
+                if(users.Count()==0)
+                    return NotFound($"Não existem usuarios com o nome {name}");
+
+                return Ok(users);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro na hora de consultar os usuarios");
+            }
+        }
+
+        [HttpGet("{id:int}", Name="GetUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            try
+            {
+                var users = await _userService.GetUser(id);
+                if (users == null)
+                    return NotFound($"Não existem usuarios com o id {id}");
+
+                return Ok(users);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Erro na hora de consultar os usuarios");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User user)
+        {
+            try
+            {
+                await _userService.CreateUser(user);
+                return CreatedAtRoute(nameof(GetUser), new { id = user.Id }, user);
+            }
+            catch 
+            {
+                return BadRequest("deu xabu");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] User user)
+        {
+            try
+            {
+                if(user.Id == id)
+                {
+                    await _userService.UpdateUser(user);
+                    return Ok($"Usuário com id = {id} foi atualizado");
+                }
+                else
+                {
+                    return BadRequest("Dado inconsistente");
+                }
+            }
+            catch
+            {
+                return BadRequest("deu xabu");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            try
+            {
+                var user = await _userService.GetUser(id);
+                if (user != null)
+                {
+                    await _userService.DeleteUser(user);
+                    return Ok($"user de id = {id} foi excluido");
+                }
+                else {
+                    return BadRequest($"user de id = {id} não encontrado");
+                }
+            }
+            catch
+            {
+                return BadRequest("deu xabu");
+            }
+        }
     }
 }
